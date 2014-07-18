f0552165fa52bfebfcfaa2c887e52aafb1f51e81
Bahrunnur <chooty.ganteng@gmail.com>
2014-07-18 23:35:03 +0700

    add encode function and moving data for convenient in the future

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,15 @@ Application Instance
 
 tiny piece of app
 """
+import base64
+
 from Flask import Flask
+
+
+def encode_niu(niu):
+    to_encode = "".join([chr(ord(x)+3) for x in niu])
+    return base64.b64encode(to_encode)
+
 
 app = Flask(__name__)
 

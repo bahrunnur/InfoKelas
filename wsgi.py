6ee32afd24b2e1e1420cc474e2edba0b5c1bd281
Bahrunnur <chooty.ganteng@gmail.com>
2014-07-21 23:44:23 +0700

    create a tiny mockup

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,9 @@ tiny piece of app
 """
 import base64
 
-from Flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+
+from models import db
 
 
 def encode_niu(niu):
@@ -15,6 +17,39 @@ def encode_niu(niu):
 
 
 app = Flask(__name__)
+
+app.config.update(dict(
+    DATABASE=db,
+    DEBUG=True,
+    SECRET_KEY='ada deh'
+))
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    niu = request.form['niu']
+    return redirect(url_for('show_kelas', niu=niu))
+
+
+@app.route('/mahasiswa/<niu>')
+def show_kelas(niu):
+    return render_template('kelas.html', niu=niu)
+
+
+@app.route('/kelas/<id>')
+def detail_kelas(id):
+    return True
+
+
+@app.route('/update')
+def update():
+    return True
+
 
 if __name__ == '__main__':
     app.run()
